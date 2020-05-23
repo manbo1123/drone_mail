a8ee78ccfd4bac49a68ce666adec10cf08d30193
manbo1123 <manbobinaccomplish@yahoo.co.jp>
2020-05-23 19:48:53 +0900

    setting_of_deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      #動作確認のため、仮置き
+      username == 'username' && password == 'password'
+    end
+  end
 end
