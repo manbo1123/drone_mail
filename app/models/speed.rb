cbc9a1a63b2c169b3926f29ec378bbde99bf9b90
manbo1123 <manbobinaccomplish@yahoo.co.jp>
2020-05-31 20:23:11 +0900

    add_auto_deploy

--- app/models/speed.rb
+++ app/models/speed.rb
@@ -1,6 +1,6 @@
 class Speed < ActiveHash::Base
   self.data = [
-    {id: 1, name: '〜50km/hh'},
+    {id: 1, name: '〜50km/h'},
     {id: 2, name: '50km/h〜100km/h'},
     {id: 3, name: '100km/h～'},
   ]
